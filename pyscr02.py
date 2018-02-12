0285b3e41b7242d7e94fe4fc3048316ef9635902
Allwyn Mendes <allwyn.mendes@revgurus.com>
2018-02-12 12:00:21 +0530

    Trying IF condition out

---
+++ pyscr02.py
@@ -0,0 +1,14 @@
+#Simple code to check is a declared variable is greater than 10 or not
+
+var1 = 8
+var2 = 11
+
+if var1 < 10 :
+	print(var1, " is less than 10")
+else:
+	print(var1, " is greater than 10")
+	
+if var2 < 10 :
+	print(var2, " is less than 10")
+else:
+	print(var2, " is greater than 10")
